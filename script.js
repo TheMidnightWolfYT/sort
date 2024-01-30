5efe2f03203480d2ee3de25a124a94f99c331c26
Zander <115725056+TheMidnightWolfYT@users.noreply.github.com>
2024-01-30 10:26:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -73,7 +73,8 @@ async function selectionSort(arr, delay = 100) {
 // Asynchronous Quick Sort with animation
 async function quickSort(arr, delay = 100, start = 0, end = arr.length - 1) {
     if (start >= end) {
-        return enableButtons();
+        enableButtons()
+        return;
     }
     let index = await partition(arr, start, end, delay);
     await Promise.all([
